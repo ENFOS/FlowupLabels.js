0528353f62aa23c3c0c8b30d9bb41f6b947343ae
Shadi Isber <sisb@myopera.com>
2013-11-27 14:15:12 -0800

    Consistency fixes

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,12 +1,16 @@
 (function(){
 	$('.FlowupLabels').FlowupLabels({
 		/*
-		 These are all the default values
-		 You may exclude any/all of these options
-		 if you won't be changing them
+		These are all the default values
+		You may exclude any/all of these options
+		if you won't be changing them
 		 */
+		// Handles the possibility of having input boxes prefilled on page load
 		feature_onInitLoad: false, 
+		
+		// Class when focusing an input
 		class_focused: 		'focused',
+		// Class when an input has text entered
 		class_populated: 	'populated'	
 	});
 })();
